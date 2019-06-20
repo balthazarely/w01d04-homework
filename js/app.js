a20db86f5d27596085640d08b6bc8f54b87a6608
Balthazar Ely <balthazarely@Balthazars-MacBook-Pro.local>
2019-06-20 15:45:24 -0600

    completed pt 1 of hw

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Verbal Questions
+// #1
+// A parameter acts as a variable inside the function
+// when it is declared. The argument is the data that
+// is passed back throught the functions when the
+// function is called
+
+// #2 Console.log will display the parameters in the
+// console, but it doesn't actually return a real value.
+// Return will actually cause the function to give a value,
+// which will be stored. 
+
+// #3 ???????
+
+
